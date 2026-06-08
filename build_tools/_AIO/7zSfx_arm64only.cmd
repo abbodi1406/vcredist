3769f5ccbd7dcb3bda132016b410868e34a5c8cf
abbodi1406 <33669284+abbodi1406@users.noreply.github.com>
2026-06-08 19:42:29 +0300

    Update build tools

--- build_tools/_AIO/7zSfx_arm64only.cmd
+++ build_tools/_AIO/7zSfx_arm64only.cmd
@@ -7,11 +7,9 @@ if exist "2008\x86\*.msi" set "_d=%_d% 2008\x86\"
 if exist "2010\x86\*.msi" set "_d=%_d% 2010\x86\"
 if exist "2012\x86\*.msi" set "_d=%_d% 2012\x86\"
 if exist "2013\x86\*.msi" set "_d=%_d% 2013\x86\"
-if exist "2022\x86\*.msi" set "_d=%_d% 2022\x86\"
 if exist "2026\x86\*.msi" set "_d=%_d% 2026\x86\"
+if exist "2026\arm64\*.msi" set "_d=%_d% 2026\arm64\"
 if exist "vbc\*.msi" set "_d=%_d% vbc\"
-if exist "vstor\*x86.msi" set "_d=%_d% vstor\vstor40_x86.msi "vstor\Common Files" "vstor\Program Files" "vstor\Win\Microsoft.NET\Framework""
-if exist "ucrt\*x86.mum" set "_d=%_d% ucrt\*x86*.mum ucrt\*x86*.cat ucrt\x86*.manifest ucrt\x86_microsoft-windows-*"
 if not defined _d (
 echo Error: None of the required directories is found
 pause
@@ -21,14 +19,14 @@ if exist "ARP.cmd" set "_d=%_d% ARP.cmd"
 if exist "Installer.cmd" set "_d=%_d% Installer.cmd"
 if exist "Uninstaller.cmd" set "_d=%_d% Uninstaller.cmd"
 set /a rnd=%random%
-if exist VisualCppRedist_AIO_x86only.exe ren VisualCppRedist_AIO_x86only.exe zVisualCppRedist_AIO_x86only_%rnd%.exe
+if exist VisualCppRedist_AIO-arm64.exe ren VisualCppRedist_AIO-arm64.exe zVisualCppRedist_AIO-arm64_%rnd%.exe
 attrib -A .\* /S /D
-7z.exe a VisualCppRedist_AIO_x86.7z ^
+7z.exe a VisualCppRedist_AIO-arm64.7z ^
 -mqs -mx -m0=BCJ2 -m1=LZMA:d26 -m2=LZMA:d19 -m3=LZMA:d19 -mb0:1 -mb0s1:2 -mb0s2:3 -bso0 ^
 %_d%
-copy /b 7zSfxMod.sfx + 7zSfxConfig86.txt + VisualCppRedist_AIO_x86.7z VisualCppRedist_AIO_x86only.exe && (
-attrib -A .\VisualCppRedist_AIO_x86.exe
-del /f /q VisualCppRedist_AIO_x86.7z
+copy /b 7zSfxMod.sfx + 7zSfxConfigArm64.txt + VisualCppRedist_AIO-arm64.7z VisualCppRedist_AIO-arm64.exe && (
+attrib -A .\VisualCppRedist_AIO-arm64.exe
+del /f /q VisualCppRedist_AIO-arm64.7z
 )
 pause
 exit /b
--- build_tools/_AIO/MSIProductCode.vbs
+++ build_tools/_AIO/MSIProductCode.vbs
@@ -2,6 +2,7 @@ For Each MSIPath in WScript.Arguments
 	Set MSIDetails = EvaluateMSI(MSIPath)
 	WScript.Echo "   Product Name: " & MSIDetails("ProductName")
 	WScript.Echo "   Product Code: " & MSIDetails("ProductCode")
+	WScript.Echo "Compressed  : " & MSIDetails("CompressedCode")
 	WScript.Echo ""
 Next
 
@@ -18,6 +19,30 @@ Function EvaluateMSI(MSIPath)
 	View.Execute
 	Set ProductCode = View.Fetch
 	objDictionary("ProductCode") = ProductCode.StringData(1)
+	objDictionary("CompressedCode") = GetCompressedGuid(ProductCode.StringData(1))
 	Set EvaluateMSI = objDictionary
 	On Error Goto 0
+End Function
+
+Function GetCompressedGuid(sGuid)
+    Dim sCompGUID
+    Dim i
+    sCompGUID = StrReverse(Mid(sGuid, 2, 8))  & _
+                StrReverse(Mid(sGuid, 11, 4)) & _
+                StrReverse(Mid(sGuid, 16, 4)) 
+    For i = 21 To 24
+	    If i Mod 2 Then
+		    sCompGUID = sCompGUID & Mid(sGuid, (i + 1), 1)
+	    Else
+		    sCompGUID = sCompGUID & Mid(sGuid, (i - 1), 1)
+	    End If
+    Next
+    For i = 26 To 37
+	    If i Mod 2 Then
+		    sCompGUID = sCompGUID & Mid(sGuid, (i - 1), 1)
+	    Else
+		    sCompGUID = sCompGUID & Mid(sGuid, (i + 1), 1)
+	    End If
+    Next
+    GetCompressedGuid = sCompGUID
 End Function
